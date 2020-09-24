758da49ad9792cc06b0e960cd8214d3a4129773f
Patrick Taylor <ptrcktylr@icloud.com>
2020-09-24 12:18:57 -0400

    tagging feature completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  include CommentsHelper
 
   def create
     @comment = Comment.new(comment_params)
@@ -7,10 +8,6 @@ class CommentsController < ApplicationController
     @comment.save
 
     redirect_to article_path(@comment.article)
-  end
-
-  def comment_params
-    params.require(:comment).permit(:author_name, :body)
   end
 
 end
